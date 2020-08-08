33e7082a6af275f299802866ac8e8412826df4f9
Yusuf Olokoba <lanre@krikey.com>
2020-08-08 18:20:17 -0400

    First commit

---
+++ Assets/ScanCam.cs
@@ -0,0 +1,41 @@
+﻿/* 
+*   NatSuite Integrations
+*   Copyright (c) 2020 Yusuf Olokoba
+*/
+
+namespace NatSuite.Integrations {
+
+    using UnityEngine;
+    using UnityEngine.UI;
+    using Devices;
+
+    public sealed class ScanCam : MonoBehaviour {
+
+        [Header(@"Camera Preview")]
+        public RawImage rawImage;
+        public AspectRatioFitter aspectFitter;
+
+        Texture2D previewTexture;
+
+        async void Start () {
+            // Request permissions
+            if (!await MediaDeviceQuery.RequestPermissions<CameraDevice>()) {
+                Debug.LogError("User failed to grant camera permissions");
+                return;
+            }
+            // Get camera
+            var query = new MediaDeviceQuery(MediaDeviceQuery.Criteria.GenericCameraDevice);
+            var cameraDevice = query.currentDevice as ICameraDevice;
+            // Start the camera preview
+            cameraDevice.previewResolution = (1280, 720);
+            previewTexture = await cameraDevice.StartRunning();
+            // Display preview
+            rawImage.texture = previewTexture;
+            aspectFitter.aspectRatio = (float)previewTexture.width / previewTexture.height;
+        }
+
+        void Update () {
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# NatDevice-ZXing.NET Integration
+This is an example project which integrates the NatDevice media device API with ZXing.NET for realtime barcode detection.
+
+*ILLUSTRATION*.
+
+## Requirements
+- Unity 2019.2+
+- NatDevice 1.0.3+
+
+## Setup Instructions
+- Download and open the project in Unity.
+- Import [NatDevice API from the Asset Store](https://assetstore.unity.com/packages/tools/integration/natdevice-media-device-api-162053).
+- Set your build target to iOS or Android.
+- Build and run!
